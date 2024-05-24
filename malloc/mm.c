d6f3b8bc501ca6e9867622a7099884f14fd7d382
Rayhub <1102290321@qq.com>
2024-05-24 09:59:17 +0800

    update malloc lab

--- malloc/mm.c
+++ malloc/mm.c
@@ -59,8 +59,8 @@ static char *heap_listp;
  * mm_init
  */
 int mm_init(void) {
-
 // Your code here
+
 }
 
 /*
@@ -75,8 +75,8 @@ void *malloc(size_t size) {
  * free
  */
 void free(void *ptr) {
-
 // Your code here
+
 }
 
 /*
@@ -145,22 +145,11 @@ bool mm_checkheap(int lineno) {
     return (bool)lineno;
 }
 
+/* Extend the heap by `words` bytes. Return a pointer to the new free block. */
+// HINT: MAKE SURE THE SIZE OF THE HEAP IS PROPERLY ALIGNED
 static void *extend_heap(size_t words) {
-    char *bp;
-    size_t size;
+// Your code here
 
-    /* Allocate an even number of words to maintain alignment */
-    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-    if ((bp = mem_sbrk(size)) == (void *)-1)
-        return NULL;
-
-    /* Initialize free block header/footer and the epilogue header */
-    PUT(HDRP(bp), PACK(size, 0));         /* Free block header */
-    PUT(FTRP(bp), PACK(size, 0));         /* Free block footer */
-    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
-
-    /* Coalesce if the previous block was free */
-    return coalesce(bp);
 }
 
 static void *coalesce(void *bp) {
@@ -172,40 +161,19 @@ static void *coalesce(void *bp) {
     if (prev_alloc && next_alloc) {
         return bp;
     }
-    size_t size = GET_SIZE(HDRP(bp));
-    /* Case 2 */
-    if (prev_alloc && !next_alloc) {
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-    }
-    /* Case 3 */
-    else if (!prev_alloc && next_alloc) {
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-        PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
-    }
-    /* Case 4 */
-    else {
-        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
-    }
-    return bp;
+    // Your code here: case 2, 3 and 4
+
 }
 
 /* First-fit search */
 // Return the first fit block, if not find, return NULL
 static void *find_fit(size_t asize) {
-   
 // Your code here
 
-   
 }
 
 // Place the block
 static void place(void *bp, size_t asize) {
 // Your code here
+
 }
